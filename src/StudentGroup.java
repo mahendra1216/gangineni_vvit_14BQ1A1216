c993321363ae5c6beb4418739937d9935fcd7488
mahendra1216 <31876835+mahendra1216@users.noreply.github.com>
2017-09-13 12:47:24 +0530

    Second Commit

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -281,6 +281,22 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student getNextStudent(Student student) {
 		// Add your implementation here
-		return null;
+		if(student==null)
+        {
+            throw new IllegalArgumentException();
+        }
+        else
+        {
+		int flag=0;
+		for(int i=0;i<=students.length-1;i++)
+        {
+            if(students[i].equals(student))
+            {
+               return students[i+1];
+            }
+        }
+        if(flag==0)
+            throw new IllegalArgumentException("Student not exist");
+	}
 	}
 }
